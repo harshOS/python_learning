2755153de7cab534576a4b4f3e65e857febb8c58
harshOS <harshithngr@gmail.com>
2020-03-28 14:45:15 +0530

    list tuple

---
+++ ABSWP/DSpractice.py
@@ -0,0 +1,14 @@
+# spam = [2, 5, 3.14, 1, -7] #list
+# spam.sort()
+# print(spam)
+# spam.sort(reverse=True)
+# print(spam)
+
+# spam = (1,2,3) #tuple
+# #spam = 0 #tuples are immutable
+# print(spam)
+
+print(type(('hello',)))
+#<class 'tuple'>
+type(('hello'))
+#<class 'str'>
