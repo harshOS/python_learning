ee8a58dd140dc8cde9e93b34671bc27d70ce3fbd
harshOS <harshithngr@gmail.com>
2020-03-29 11:33:53 +0530

    strings

--- ABSWP/stringsPractice.py
+++ ABSWP/stringsPractice.py
@@ -1,12 +1,28 @@
-while True:
-    print('Enter your age:')
-    age = input()
-    if age.isdecimal():
-        break
-    print('Please enter a number for your age.')
-while True:
-    print('Select a new password (letters and numbers only):')
-    password = input()
-    if password.isalnum():
-        break
-    print('Passwords can only have letters and numbers.')
+# while True:
+#     print('Enter your age:')
+#     age = input()
+#     if age.isdecimal():
+#         break
+#     print('Please enter a number for your age.')
+# while True:
+#     print('Select a new password (letters and numbers only):')
+#     password = input()
+#     if password.isalnum():
+#         break
+#     print('Passwords can only have letters and numbers.')
+
+# print(', '.join(['cats', 'rats', 'bats']))
+# print(' '.join(['My', 'name', 'is', 'Simon']))
+# print('ABC'.join(['My', 'name', 'is', 'Simon']))
+#
+# print('MyABCnameABCisABCSimon'.split('ABC'))
+# print('My name is Simon'.split(' '))
+
+print('Hello'.rjust(20))
+print('Hello'.rjust(20, '*'))
+
+print('Hello'.ljust(20))
+print('Hello'.ljust(20, '-'))
+
+print('Hello'.center(20))
+print('Hello'.center(20, '='))
