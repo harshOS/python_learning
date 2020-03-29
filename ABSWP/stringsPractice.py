aaac0eeafe8032b1940374b9caa3a96325700db7
harshOS <harshithngr@gmail.com>
2020-03-29 11:38:36 +0530

    strings

--- ABSWP/stringsPractice.py
+++ ABSWP/stringsPractice.py
@@ -18,11 +18,20 @@
 # print('MyABCnameABCisABCSimon'.split('ABC'))
 # print('My name is Simon'.split(' '))
 
-print('Hello'.rjust(20))
-print('Hello'.rjust(20, '*'))
+# print('Hello'.rjust(20))
+# print('Hello'.rjust(20, '*'))
+#
+# print('Hello'.ljust(20))
+# print('Hello'.ljust(20, '-'))
+#
+# print('Hello'.center(20))
+# print('Hello'.center(20, '='))
 
-print('Hello'.ljust(20))
-print('Hello'.ljust(20, '-'))
+def printPicnic(itemsDict, leftWidth, rightWidth):
+    print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
+    for k, v in itemsDict.items():
+        print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
 
-print('Hello'.center(20))
-print('Hello'.center(20, '='))
+picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
+printPicnic(picnicItems, 12, 5)
+printPicnic(picnicItems, 20, 6)
