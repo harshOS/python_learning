95dcbad72826ca2250d5e4d772c6de65ba6ddf5e
harshOS <harshithngr@gmail.com>
2020-03-30 17:32:45 +0530

    initial

--- ABSWP/phoneNumINRegex.py
+++ ABSWP/phoneNumINRegex.py
@@ -1,5 +1,5 @@
 import re
 
-isPhoneIn = re.compile(r'[+91]\d\d\d\d\d\d\d\d\d\d\d\d')
+isPhoneIn = re.compile(r'(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[789]\d{9}')
 mo = isPhoneIn.search("wall det +917894561230")
 print("Phone number found: "+mo.group())
