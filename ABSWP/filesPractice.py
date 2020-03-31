3bb1406fbcfbbe2012011b4c1b1ca376571d5b98
harshOS <harshithngr@gmail.com>
2020-03-31 18:28:07 +0530

    initial

--- ABSWP/filesPractice.py
+++ ABSWP/filesPractice.py
@@ -27,4 +27,21 @@ print(os.getcwd())
 # 'C:\\Users\\Harshith NG\\PycharmProjects\\python_learning'
 # >>> os.path.split('C:\\Users\\Harshith NG\\PycharmProjects\\python_learning\\ABSWP')
 # ('C:\\Users\\Harshith NG\\PycharmProjects\\python_learning', 'ABSWP')
-# >>>
+# >>> os.path.sep
+# '\\'
+# >>> os.path.getsize()
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# TypeError: getsize() missing 1 required positional argument: 'filename'
+# >>> os.path.getsize(os.getcwd())
+# 4096
+# >>> os.listdir()
+# ['bulletAdder.py', 'collatzSeries.py', 'DSpractice.py', 'filesPractice.py', 'isPhoneNumber.py', 'isPhoneNumberIN.py', 'password_locker.py', 'phoneAndemail.py', 'phoneNumINRegex.py', 'README.md', 'stringsPractice.py', 'test']
+# >>> os.path.getsize('..')
+# 4096
+# >>> os.path.exists('test')
+# True
+# >>> os.path.isfile('test')
+# False
+# >>> os.path.isdir('test')
+# True
