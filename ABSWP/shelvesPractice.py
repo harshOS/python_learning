efc0ed4d6138db2685ea2596b11beaa80d1b8e08
harshOS <harshithngr@gmail.com>
2020-04-01 12:44:56 +0530

    initial

--- ABSWP/shelvesPractice.py
+++ ABSWP/shelvesPractice.py
@@ -6,6 +6,12 @@ shelfFile.close()
 
 file = shelve.open('mydata')
 print(file['cats'])
+print(type(file))
+print(list(file.keys()))
+print(list(file.items()))
+print(list(file.values()))
+
 file.close()
+
 
 
